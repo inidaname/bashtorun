dd4c11ddd78c5dea0e2da4961013627b72dbd401
Hassan Sani <saniyhassan@gmail.com>
2021-06-01 21:57:06 +0100

    just edited directory

--- yet.sh
+++ yet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This is a comment
 
-gnome-terminal --tab -- yarn --cwd ./news-api/ develop && gnome-terminal --tab -- yarn --cwd ./news-frontend start
+gnome-terminal --tab -- yarn --cwd ../news-api/ develop && gnome-terminal --tab -- yarn --cwd ../news-frontend start
